0c6a8fff348e055bdf0bffdcc00f391fae9a45d7
can.shen <can.shen@mobvista.com>
2021-12-09 14:11:43 +0800

    清理https证书

--- setup/efk/es/install.sh
+++ setup/efk/es/install.sh
@@ -4,8 +4,6 @@ namespace="spotmax-maxcloud"
 
 ####导入证书
 kubectl create secret generic elastic-certificates --from-file=elastic-certificates.p12 -n $namespace 
-kubectl create secret generic elastic-certificate-pem --from-file=elastic-certificate.pem  -n $namespace
-kubectl create secret generic elastic-certificate-crt --from-file=elastic-certificate.crt  -n $namespace
 
 ####设置es用户名和密码
 kubectl create secret  generic elastic-credentials --from-literal=username=elastic --from-literal=password=elastic -n $namespace
