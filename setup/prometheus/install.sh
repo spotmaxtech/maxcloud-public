7e84f47513dfcff0b15a96f644b7414aece0fa84
can.shen <can.shen@mobvista.com>
2021-12-28 17:12:19 +0800

    修复kube-prometheus-stack 新版本有bug问题

--- setup/prometheus/install.sh
+++ setup/prometheus/install.sh
@@ -5,11 +5,11 @@ namespace="spotmax-maxcloud"
 #安装 kube-prometheus-statck
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm install kube-prometheus-stack -f values.yaml  prometheus-community/kube-prometheus-stack  -n $namespace
+helm install kube-prometheus-stack  prometheus-community/kube-prometheus-stack  -n $namespace  --version 21.0.0
 
 
 
 #Isito 监控数据采集
-kubectl apply -f ServiceMonitor.yaml   -n $namespace
-kubectl apply -f IstioControlPlaneMetrics.yaml   -n $namespace
+kubectl apply -f ServiceMonitor.yaml  
+kubectl apply -f IstioControlPlaneMetrics.yaml -n $namespace 
 kubectl apply -f IsitoDataPlaneMetrics.yaml   -n $namespace
